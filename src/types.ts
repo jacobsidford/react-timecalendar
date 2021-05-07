a0f7da5e95c8f1966184420c5b088246e43b5793
Jacob Sidford <jacob.sidford@gmail.com>
2021-05-07 10:26:03 +1000

    changed typings to include optionals

--- src/types.ts
+++ src/types.ts
@@ -27,22 +27,22 @@ export interface DayProps {
 
   export interface TimeCalendarProps {
     openHours: number[][],
-    bookings: Booking[],
-    disableHistory: boolean,
-    clickable: boolean,
-    timeSlot: number,
+    bookings?: Booking[],
+    disableHistory?: boolean,
+    clickable?: boolean,
+    timeSlot?: number,
     onDateFunction: Function,
     onTimeClick: Function,
     selectedTime: SelectedTime
   }
 
   export interface TimeSelectProps {
-    disableHistory: boolean,
+    disableHistory?: boolean,
     selectedDate: Date,
     timeSlot: number,
     openHours: number[][],
     onTimeClick: Function,
-    bookings: Booking[],
+    bookings?: Booking[],
     selectedTime: SelectedTime
   }
 
@@ -54,10 +54,10 @@ export interface DayProps {
 
   export interface WeeksProps {
     selectedDate: Date,
-    bookings: Booking[],
-    timeSlot: number;
+    bookings?: Booking[],
+    timeSlot?: number;
     onDateClick: Function,
-    clickable: boolean,
-    disableHistory: boolean,
+    clickable?: boolean,
+    disableHistory?: boolean,
     selectedTime: SelectedTime
   }
