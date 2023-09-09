baf5c00fafed978a44f4a32ed1308e027488e174
Matteo Soresini <matteo.soresini@hotmail.it>
2023-09-09 18:07:27 +0200

    translating to italian

--- src/DayTitle.tsx
+++ src/DayTitle.tsx
@@ -1,51 +1,55 @@
 import React from "react";
 import dateFns from "date-fns";
+import it from "date-fns/locale/it";
 import { DayTitleProps } from "./types";
 
 type DayTitleState = {
-  dateFormat: string;
+    dateFormat: string;
 };
 
 export default class DayTitle extends React.Component<
-  DayTitleProps,
-  DayTitleState
+    DayTitleProps,
+    DayTitleState
 > {
-  constructor(props: DayTitleProps) {
-    super(props);
-    this.state = {
-      dateFormat: "ddd",
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      // @ts-ignore
-      dateFormat: this.dayTitle.parentNode.clientWidth > 500 ? "dddd" : "ddd",
-    });
-  }
-
-  render() {
-    const { currentMonth } = this.props;
-    const { dateFormat } = this.state;
-    const days = [];
-    const startDate = dateFns.startOfWeek(currentMonth);
-    for (let i = 0; i < 7; i++) {
-      days.push(
-        <div className="col col-center" key={i}>
-          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
-        </div>
-      );
+    constructor(props: DayTitleProps) {
+        super(props);
+        this.state = {
+            dateFormat: "ddd"
+        };
     }
-    return (
-      <div
-        className="days row"
-        ref={(c) => {
-          // @ts-ignore
-          this.dayTitle = c;
-        }}
-      >
-        {days}
-      </div>
-    );
-  }
+
+    componentDidMount() {
+        this.setState({
+            // @ts-ignore
+            dateFormat:
+                this.dayTitle.parentNode.clientWidth > 500 ? "dddd" : "ddd"
+        });
+    }
+
+    render() {
+        const { currentMonth } = this.props;
+        const { dateFormat } = this.state;
+        const days = [];
+        const startDate = dateFns.startOfWeek(currentMonth);
+        for (let i = 0; i < 7; i++) {
+            days.push(
+                <div className="col col-center" key={i}>
+                    {dateFns.format(dateFns.addDays(startDate, i), dateFormat, {
+                        locale: it
+                    })}
+                </div>
+            );
+        }
+        return (
+            <div
+                className="days row"
+                ref={(c) => {
+                    // @ts-ignore
+                    this.dayTitle = c;
+                }}
+            >
+                {days}
+            </div>
+        );
+    }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 //@ts-nocheck
 import React, { PureComponent } from "react";
 import dateFns from "date-fns";
+import it from "date-fns/locale/it";
 import Header from "./Header";
 import Weeks from "./Weeks";
 import TimeSelect from "./TimeSelect";
@@ -8,127 +9,136 @@ import { TimeCalendarProps } from "./types";
 import "./App.scss";
 
 type TimeCalendarState = {
-  selectedDate: Date;
-  timeSelect: boolean;
+    selectedDate: Date;
+    timeSelect: boolean;
 };
 
 export default class TimeCalendar extends PureComponent<
-  TimeCalendarProps,
-  TimeCalendarState
+    TimeCalendarProps,
+    TimeCalendarState
 > {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedDate: new Date(),
-      timeSelect: false,
-    };
-    this.onDateClick = this.onDateClick.bind(this);
-    this.nextTime = this.nextTime.bind(this);
-    this.prevTime = this.prevTime.bind(this);
-    this.timeSelectToggle = this.timeSelectToggle.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedDate: new Date(),
+            timeSelect: false
+        };
+        this.onDateClick = this.onDateClick.bind(this);
+        this.nextTime = this.nextTime.bind(this);
+        this.prevTime = this.prevTime.bind(this);
+        this.timeSelectToggle = this.timeSelectToggle.bind(this);
+    }
 
-  onDateClick(day: Date) {
-    const { onDateFunction } = this.props;
-    this.setState({
-      selectedDate: day,
-    });
-    if (onDateFunction) onDateFunction(day);
-  }
+    onDateClick(day: Date) {
+        const { onDateFunction } = this.props;
+        this.setState({
+            selectedDate: day
+        });
+        if (onDateFunction) onDateFunction(day);
+    }
 
-  nextTime() {
-    const { selectedDate, timeSelect } = this.state;
-    this.setState({
-      selectedDate: timeSelect
-        ? dateFns.addDays(selectedDate, 1)
-        : dateFns.addMonths(selectedDate, 1),
-    });
-  }
+    nextTime() {
+        const { selectedDate, timeSelect } = this.state;
+        this.setState({
+            selectedDate: timeSelect
+                ? dateFns.addDays(selectedDate, 1)
+                : dateFns.addMonths(selectedDate, 1)
+        });
+    }
 
-  prevTime() {
-    const { selectedDate, timeSelect } = this.state;
-    const { disableHistory } = this.props;
+    prevTime() {
+        const { selectedDate, timeSelect } = this.state;
+        const { disableHistory } = this.props;
 
-    if (
-      disableHistory &&
-      ((dateFns.isPast(dateFns.startOfMonth(selectedDate)) && !timeSelect) ||
-        (dateFns.isPast(dateFns.startOfDay(selectedDate)) && timeSelect))
-    )
-      return;
+        if (
+            disableHistory &&
+            ((dateFns.isPast(dateFns.startOfMonth(selectedDate)) &&
+                !timeSelect) ||
+                (dateFns.isPast(dateFns.startOfDay(selectedDate)) &&
+                    timeSelect))
+        )
+            return;
 
-    this.setState({
-      selectedDate: timeSelect
-        ? dateFns.subDays(selectedDate, 1)
-        : dateFns.subMonths(selectedDate, 1),
-    });
-  }
+        this.setState({
+            selectedDate: timeSelect
+                ? dateFns.subDays(selectedDate, 1)
+                : dateFns.subMonths(selectedDate, 1)
+        });
+    }
 
-  timeSelectToggle() {
-    const { timeSelect } = this.state;
-    this.setState({
-      timeSelect: !timeSelect,
-    });
-  }
+    timeSelectToggle() {
+        const { timeSelect } = this.state;
+        this.setState({
+            timeSelect: !timeSelect
+        });
+    }
 
-  render() {
-    const {
-      disableHistory,
-      timeSlot,
-      openHours,
-      onTimeClick,
-      bookings,
-      startTime,
-      endTime,
-      clickable,
-    } = this.props;
-    const { selectedDate, timeSelect } = this.state;
-    return (
-      <div className="calendar">
-        <Header
-          selectedDate={
-            !timeSelect
-              ? dateFns.format(selectedDate, "MMMM YYYY")
-              : dateFns.format(selectedDate, "dddd Do MMMM")
-          }
-          nextTime={this.nextTime}
-          prevTime={this.prevTime}
-          timeSelect={timeSelect}
-        />
-        {timeSelect ? (
-          <>
-            <TimeSelect
-              selectedDate={selectedDate}
-              disableHistory={disableHistory}
-              timeSlot={timeSlot}
-              openHours={openHours}
-              onTimeClick={onTimeClick}
-              bookings={bookings}
-              selectedTime={{start: startTime, end: endTime}}
-            />
-          </>
-        ) : (
-          <>
-            <Weeks
-              selectedDate={selectedDate}
-              disableHistory={disableHistory}
-              onDateClick={this.onDateClick}
-              bookings={bookings}
-              timeSlot={timeSlot}
-              clickable={clickable}
-              selectedTime={{start: startTime, end: endTime}}
-            />
-          </>
-        )}
-        {timeSlot && openHours && (
-          <button
-            className="timeSelector"
-            onClick={this.timeSelectToggle}
-            type="button"
-          >
-            <p>{timeSelect ? "Select Day" : "Select Time"}</p>
-          </button>
-        )}
-      </div>
-    );
-  }
+    render() {
+        const {
+            disableHistory,
+            timeSlot,
+            openHours,
+            onTimeClick,
+            bookings,
+            startTime,
+            endTime,
+            clickable
+        } = this.props;
+        const { selectedDate, timeSelect } = this.state;
+        return (
+            <div className="calendar">
+                <Header
+                    selectedDate={
+                        (!timeSelect
+                            ? dateFns.format(selectedDate, "MMMM YYYY", {
+                                  locale: it
+                              })
+                            : dateFns.format(selectedDate, "dddd Do MMMM"),
+                        { locale: it })
+                    }
+                    nextTime={this.nextTime}
+                    prevTime={this.prevTime}
+                    timeSelect={timeSelect}
+                />
+                {timeSelect ? (
+                    <>
+                        <TimeSelect
+                            selectedDate={selectedDate}
+                            disableHistory={disableHistory}
+                            timeSlot={timeSlot}
+                            openHours={openHours}
+                            onTimeClick={onTimeClick}
+                            bookings={bookings}
+                            selectedTime={{ start: startTime, end: endTime }}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <Weeks
+                            selectedDate={selectedDate}
+                            disableHistory={disableHistory}
+                            onDateClick={this.onDateClick}
+                            bookings={bookings}
+                            timeSlot={timeSlot}
+                            clickable={clickable}
+                            selectedTime={{ start: startTime, end: endTime }}
+                        />
+                    </>
+                )}
+                {timeSlot && openHours && (
+                    <button
+                        className="timeSelector"
+                        onClick={this.timeSelectToggle}
+                        type="button"
+                    >
+                        <p>
+                            {timeSelect
+                                ? "Scegli un'altra data"
+                                : "Select Time"}
+                        </p>
+                    </button>
+                )}
+            </div>
+        );
+    }
 }
